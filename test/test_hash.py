08ee3bcb9dabf6c764757fa6856997ae40b9f000
AdrianHarrison <harrison.adrian.85@gmail.com>
2019-06-14 23:05:26 -0500

    Testing invalid salts

--- test/test_hash.py
+++ test/test_hash.py
@@ -26,6 +26,16 @@ class TestEncryption(unittest.TestCase):
             message = bytes(message)
             self.assertFalse(hasher.validate(message, cryptoblock))
 
+    def test_invalid_scrypt_salt(self):
+        for _ in range(10):
+            salt = os.urandom(32)
+            message = os.urandom(32)
+            hasher = Hasher(CryptoSrcEnum.CRYPTO, salt)
+            cryptoblock = hasher.hash(message)[:len(salt)]
+            salt = os.urandom(32)
+            hasher = Hasher(CryptoSrcEnum.CRYPTO, salt)
+            self.assertFalse(hasher.validate(message, cryptoblock))
+
 
     def test_valid_nacl_hash(self):
         for _ in range(10):
